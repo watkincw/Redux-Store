8b33fc9a385c357fc636d69654eb89b7bfca01c2
Colton Watkins <83984833+ThManHimself@users.noreply.github.com>
2021-10-27 16:18:37 -0500

    deploying

---
+++ client/build/precache-manifest.fcc92b509141dfacc8cdca33a6e64243.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b0eb0e95d05a75916e5f9ea590800e05",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f5be5922dcf283444641",
+    "url": "/static/css/main.574d32c2.chunk.css"
+  },
+  {
+    "revision": "45a213976e9185788c92",
+    "url": "/static/js/2.b78fdd88.chunk.js"
+  },
+  {
+    "revision": "aa4100970f46d7e0ec2af84de3f2740b",
+    "url": "/static/js/2.b78fdd88.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f5be5922dcf283444641",
+    "url": "/static/js/main.a9e346a6.chunk.js"
+  },
+  {
+    "revision": "02751e160f988a50d44f",
+    "url": "/static/js/runtime-main.8a258a78.js"
+  },
+  {
+    "revision": "ab4978553bd68ca15e503a0da7fa604d",
+    "url": "/static/media/spinner.ab497855.gif"
+  }
+]);
